ec9e3aef906ae86ad689bf92c3e24b2b30786cd4
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2023-04-05 17:30:23 -0400

    🎉 Initial Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Particle from './Particle.js';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// if(window.location.protocol === "http:") window.location.protocol = "https:"
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
